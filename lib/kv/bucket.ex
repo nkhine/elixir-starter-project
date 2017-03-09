c784b759581ccb57349243375d5c98fd61daab7d
Norman Khine <norman@khine.net>
2017-03-09 21:31:54 +0000

    add delete bucket and pass test

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -18,4 +18,15 @@ defmodule KV.Bucket do
   def put(b, k, v) do
     Agent.update(b, &Map.put(&1, k, v))
   end
+  @doc """
+  Besides getting a value and updating the agent state, agents allow us to get
+  a value and update the agent state in one function call via
+  Agent.get_and_update/2
+  Deletes `key` from `bucket`.
+
+  Returns the current value of `key`, if `key` exists.
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
+  end
 end
--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -3,7 +3,12 @@ defmodule KV.BucketTest do
 
   setup do
     {:ok, b} = KV.Bucket.start_link
-    #return key:value
+    # Note that we need a mechanism to pass the bucket pid from the callback
+    # to the test. We do so by using the test context. When we return
+    # {:ok, bucket: bucket} from the callback, ExUnit will merge the second
+    # element of the tuple (a dictionary) into the test context.
+    # The test context is a map which we can then match in the test definition,
+    # providing access to these values inside the block test:
     {:ok, b: b}
   end
   test "stores values by key", %{b: b} do
@@ -12,5 +17,7 @@ defmodule KV.BucketTest do
 
     KV.Bucket.put(b, "transcript", 3)
     assert KV.Bucket.get(b, "transcript") == 3
+    KV.Bucket.delete(b, "transcript")
+    assert KV.Bucket.get(b, "transcript") == nil
   end
 end
